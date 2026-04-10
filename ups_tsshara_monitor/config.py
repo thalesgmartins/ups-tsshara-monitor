3b891bfabddf46a2c433c65b95f0210905f41a3f
thalesgmartins <thalesdiniz30@gmail.com>
2026-04-09 23:10:57 -0300

    add nome do server e melhorando docs

--- ups_tsshara_monitor/config.py
+++ ups_tsshara_monitor/config.py
@@ -1,11 +1,17 @@
 import os
 
+# Configurações do Servidor
+SERVER_NAME = os.getenv("SERVER_NAME", "ups_tsshara_monitor")
+
 # Configurações do MQTT
 MQTT_HOST   = os.getenv("MQTT_HOST")
 MQTT_PORT   = int(os.getenv("MQTT_PORT", 1883))
 MQTT_USER   = os.getenv("MQTT_USER")
 MQTT_PASS   = os.getenv("MQTT_PASS")
-MQTT_PREFIX = os.getenv("MQTT_PREFIX")
+
+# Tópicos MQTT
+MQTT_PREFIX = os.getenv("MQTT_PREFIX", "homeassistant/sensor")
+MQTT_TOPIC = f"{MQTT_PREFIX.rstrip('/')}/{SERVER_NAME}"
 
 # Configurações do Serial
 PORT        = os.getenv("SERIAL_PORT","/dev/ttyTSSHARA0")
