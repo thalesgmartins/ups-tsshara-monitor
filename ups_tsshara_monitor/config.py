67974bcd27a38d2e4044f4a6934ab5cb91ef227f
thalesgmartins <thalesdiniz30@gmail.com>
2026-07-31 17:01:44 -0300

    correções de formatação com o ruff

--- ups_tsshara_monitor/config.py
+++ ups_tsshara_monitor/config.py
@@ -4,17 +4,17 @@ import os
 SERVER_NAME = os.getenv("SERVER_NAME", "ups_tsshara_monitor")
 
 # Configurações do MQTT
-MQTT_HOST   = os.getenv("MQTT_HOST")
-MQTT_PORT   = int(os.getenv("MQTT_PORT", 1883))
-MQTT_USER   = os.getenv("MQTT_USER")
-MQTT_PASS   = os.getenv("MQTT_PASS")
+MQTT_HOST = os.getenv("MQTT_HOST")
+MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
+MQTT_USER = os.getenv("MQTT_USER")
+MQTT_PASS = os.getenv("MQTT_PASS")
 
 # Tópicos MQTT
 MQTT_PREFIX = os.getenv("MQTT_PREFIX", "homeassistant/sensor")
 MQTT_TOPIC = f"{MQTT_PREFIX.rstrip('/')}/{SERVER_NAME}"
 
 # Configurações do Serial
-PORT        = os.getenv("SERIAL_PORT","/dev/ttyTSSHARA0")
-BAUD        = int(os.getenv("SERIAL_BAUD", 9600))
-SLAVE_ID    = int(os.getenv("SERIAL_SLAVE_ID", 1))
-POLL_SECS   = int(os.getenv("SERIAL_POLL_SECS", 5))
+PORT = os.getenv("SERIAL_PORT", "/dev/ttyTSSHARA0")
+BAUD = int(os.getenv("SERIAL_BAUD", 9600))
+SLAVE_ID = int(os.getenv("SERIAL_SLAVE_ID", 1))
+POLL_SECS = int(os.getenv("SERIAL_POLL_SECS", 5))
