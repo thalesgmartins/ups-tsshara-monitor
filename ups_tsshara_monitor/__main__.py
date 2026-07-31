cb330555f74a8ae2c21abde643ad34afe1ceacbc
thalesgmartins <thalesdiniz30@gmail.com>
2026-07-31 17:02:09 -0300

    correções de formatação com o ruff

--- ups_tsshara_monitor/__main__.py
+++ ups_tsshara_monitor/__main__.py
@@ -5,22 +5,25 @@ import logging
 import threading
 import time
 
-from .poller import poll_loop
 from .mqtt import mqtt_loop
+from .poller import poll_loop
 
 VERMELHO = "\033[31m"
 VERDE = "\033[32m"
-AZUL= "\033[34m"
+AZUL = "\033[34m"
 RESET = "\033[0m"
 
-logging.basicConfig(level=logging.INFO, format=f"{VERDE}%(asctime)s{RESET} | {VERMELHO}%(levelname)s{RESET} | {AZUL}%(filename)s:%(lineno)d{RESET} | %(message)s")
+logging.basicConfig(
+    level=logging.INFO,
+    format=f"{VERDE}%(asctime)s{RESET} | {VERMELHO}%(levelname)s{RESET} | {AZUL}%(filename)s:%(lineno)d{RESET} | %(message)s",
+)
 _LOGGER = logging.getLogger(__name__)
 
 
 def main():
     # parser de argumentos
     parser = argparse.ArgumentParser(description="Tsshara UPS SYAL IN monitor")
-    parser.add_argument("--debug",  action="store_true", help="Log verboso")
+    parser.add_argument("--debug", action="store_true", help="Log verboso")
     args = parser.parse_args()
 
     if args.debug:
@@ -31,11 +34,15 @@ def main():
     state_lock = threading.Lock()
 
     # Cria a inicia a thread do Polling Serial
-    t_poll = threading.Thread(target=poll_loop, args=(shared_state, state_lock), daemon=True, name="poll")
+    t_poll = threading.Thread(
+        target=poll_loop, args=(shared_state, state_lock), daemon=True, name="poll"
+    )
     t_poll.start()
 
     # Cria a inicia a thread do Mqtt
-    t_mqtt = threading.Thread(target=mqtt_loop, args=(shared_state, state_lock), daemon=True, name="mqtt")
+    t_mqtt = threading.Thread(
+        target=mqtt_loop, args=(shared_state, state_lock), daemon=True, name="mqtt"
+    )
     t_mqtt.start()
 
     _LOGGER.info("TS Shara UPS SYAL IN Monitor Iniciado com Sucesso")
@@ -44,6 +51,7 @@ def main():
             time.sleep(1)
     except Exception as e:
         _LOGGER.info("Encerrando. Motivo: %s", e)
+
 
 if __name__ == "__main__":
     """Start TS Shara Monitor."""
