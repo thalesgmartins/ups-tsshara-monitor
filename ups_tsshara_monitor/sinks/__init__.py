ccdb7d6b245ac4e9aa32b0e8d37591b1429231e5
thalesgmartins <thalesdiniz30@gmail.com>
2026-08-03 00:37:22 -0300

    importa o mqtt no __init__ para executar o @register_sink()

--- ups_tsshara_monitor/sinks/__init__.py
+++ ups_tsshara_monitor/sinks/__init__.py
@@ -1,1 +1,5 @@
-"""Init dos Sinks para Saída de Dados."""
+"""Pacote de sinks. Importar os sinks internos aqui faz o @register_sink
+disparar, populando o registry antes de build_sinks ser chamado.
+"""
+
+from . import mqtt as _mqtt  # noqa: F401  (efeito colateral: registra "mqtt")
